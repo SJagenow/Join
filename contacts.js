ed56d8d79644fe6fc238f8878e9fc4b3e8325c11
Pascal Thelke <p.thelke@googlemail.com>
2024-02-03 15:24:00 +0100

    html5 edits to 'add contact' overlay

--- contacts.js
+++ contacts.js
@@ -2,9 +2,9 @@ const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M
 let contactList = [];
 
 /**
- *  This function is used to render an empty contactlist with alphabethical rows
+ *  This function is used to render an empty contactlist with alphabethical rows.
  * 
- * @param {string} singleLetter - This is the letter for each row in the contactlist
+ * @param {string} singleLetter - This is the letter for each row in the contactlist.
  */
 function renderContactList() {
     document.getElementById('contact_list').innerHTML = ``;
@@ -16,15 +16,13 @@ function renderContactList() {
     </div>
     `;
     for (let i = 0; i < alphabet.length; i++) {
-        const singleLetter = alphabet[i]; // id's to render alphabet and for every letter and id to render it in
+        const singleLetter = alphabet[i]; // id's to render alphabet and for every letter and id to render it in.
         document.getElementById('contact_list').innerHTML += `
         <div id="contactlist_alphabet_sorting_container${i}"> 
         ${singleLetter}  
         </div>
         <div class="divide_container">
-            <svg xmlns="http://www.w3.org/2000/svg" width="354" height="2" viewBox="0 0 354 2" fill="none">
-            <path d="M1 1H353" stroke="#D1D1D1" stroke-linecap="round"/>
-            </svg>
+        <img src="./assets/contactbook/icons_contactbook/Vector 10.svg" alt="">
         </div>
         <div id="contact_list_names${i}">
         <div class="contact_list_container">
@@ -44,32 +42,32 @@ function renderContactList() {
 }
 
 /**
- * This function is used to show the add contact overlay by clicking the 'add new contact' button 
+ * This function is used to show the add contact overlay by clicking the 'add new contact' button.
  */
 function showAddContactDialog(){
     document.getElementById('contactlist_overlay_container').style.display = 'unset';
 }
 
 /**
- * This function is given the backgorundcontainer of the overlay to close it by clicking in the background of the overlay
+ * This function is given the backgorundcontainer of the overlay to close it by clicking in the background of the overlay.
  */
 function closeAddContactDialog(){
     document.getElementById('contactlist_overlay_container').style.display = 'none';
 }
 
 /**
- * This function is used to stop the overlay from closing by clicking in it, because its a childcontainer of the clickable 'to close' container in the background
+ * This function is used to stop the overlay from closing by clicking in it, because its a childcontainer of the clickable 'to close' container in the background.
  * 
- * @param {string} event -  In this case its the Mouseclick on the Element thats a child of the 'to close' container in the background
+ * @param {string} event -  In this case its the Mouseclick on the Element thats a child of the 'to close' container in the background.
  */
 function noClose(event) {
     event.stopPropagation();
   }
 
 /**
- * This function creates a json from the inputs of the 'add contact' overlay and pushes it into the contactlist Array
+ * This function creates a json from the inputs of the 'add contact' overlay and pushes it into the contactlist Array.
  * 
- * @param {string} contact - This is the json with all informations from the inputfield. It will be pushed into contactlist
+ * @param {string} contact - This is the json with all informations from the inputfield. It will be pushed into contactlist.
  */
 function addToContacts(){
     let name = document.getElementById('contactlist_name_input');
@@ -82,8 +80,20 @@ function addToContacts(){
     };
     contactList.push(contact);
     console.log('updated contactlist:' ,contactList);
+    resetAddContactForm(name, mail, phone);
+} 
+
+/**
+ * This function is just to reset the inputfields of the 'add contact' overlay.
+ * 
+ * @param {string} name - It's the inputfield where the name is written in.
+ * @param {string} mail - It's the inputfield where the e-mail is written in.
+ * @param {string} phone - It's the inputfield where the phonenumber is written in.
+ */
+
+function resetAddContactForm(name, mail, phone){
     name.value = '';
     mail.value = '';
     phone.value = '';
     closeAddContactDialog();
-} 
+}
