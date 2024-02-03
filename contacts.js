dd9dcb31fb2f23509c75d931d6a00b8b85ab7f03
Pascal Thelke <p.thelke@googlemail.com>
2024-02-03 14:18:18 +0100

    css contactbook + first JS to create json array for contactlist + created storage token! + implemented storage.js on all html heads [KEY IS GENERATED]

--- contacts.js
+++ contacts.js
@@ -1,5 +1,11 @@
 const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let contactList = [];
 
+/**
+ *  This function is used to render an empty contactlist with alphabethical rows
+ * 
+ * @param {string} singleLetter - This is the letter for each row in the contactlist
+ */
 function renderContactList() {
     document.getElementById('contact_list').innerHTML = ``;
     document.getElementById('contact_list').innerHTML = `
@@ -37,14 +43,47 @@ function renderContactList() {
     }
 }
 
+/**
+ * This function is used to show the add contact overlay by clicking the 'add new contact' button 
+ */
 function showAddContactDialog(){
     document.getElementById('contactlist_overlay_container').style.display = 'unset';
 }
 
+/**
+ * This function is given the backgorundcontainer of the overlay to close it by clicking in the background of the overlay
+ */
 function closeAddContactDialog(){
     document.getElementById('contactlist_overlay_container').style.display = 'none';
 }
 
+/**
+ * This function is used to stop the overlay from closing by clicking in it, because its a childcontainer of the clickable 'to close' container in the background
+ * 
+ * @param {string} event -  In this case its the Mouseclick on the Element thats a child of the 'to close' container in the background
+ */
 function noClose(event) {
     event.stopPropagation();
   }
+
+/**
+ * This function creates a json from the inputs of the 'add contact' overlay and pushes it into the contactlist Array
+ * 
+ * @param {string} contact - This is the json with all informations from the inputfield. It will be pushed into contactlist
+ */
+function addToContacts(){
+    let name = document.getElementById('contactlist_name_input');
+    let mail = document.getElementById('contactlist_mail_input');
+    let phone = document.getElementById('contactlist_phone_input');
+    let contact = {
+            "name": name.value,
+            "mail": mail.value,
+            "phone": phone.value
+    };
+    contactList.push(contact);
+    console.log('updated contactlist:' ,contactList);
+    name.value = '';
+    mail.value = '';
+    phone.value = '';
+    closeAddContactDialog();
+} 
