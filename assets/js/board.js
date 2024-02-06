1591c515c1a4f118e5d6799b69b58a04defc4ccd
Stefan Jaganow <sjagenow@web.de>
2024-02-07 00:10:09 +0100

    todos design

--- assets/js/board.js
+++ assets/js/board.js
@@ -81,10 +81,33 @@ function startDragging(id) {
 }
 
 
-function generateTodo(clean) {
-    return `<div draggable="true" ondragstart="startDragging(${clean['id']})" class="todo"><div class"card_label">${clean['label']}</div> <div class"card_title">${clean['title']}</div><div class"card_description">${clean['description']}</div></div>`;
-}
 
+function generateTodo(clean) {
+    let subtaskCount = 2; // Anzahl der Subtasks
+    let progressWidth = (1 / subtaskCount) * 100; // Breite der Fortschrittsanzeige in Prozent
+
+    return `<div draggable="true" ondragstart="startDragging(${clean['id']})" class="todo">
+        <div class="card_label">${clean['label']}</div>
+        <div class="card_title">${clean['title']}</div>
+        <div class="card_description">${clean['description']}</div>
+        <div id="myProgress">
+            <div id="myBar" style="width: ${progressWidth}%;"></div><div><span>Subtask 1/2</span></div>
+        </div>
+        <div class="member_flex">
+            <div class="circle">FF</div>
+            <div class="circle_two">GG</div>
+            <div class="circle_three">WP</div>
+            <div class="circle_four">CU</div>
+            <div class="circle_five">CU</div>
+
+            <div class="prio_icon_containers">
+                <svg width="22" height="20">
+                  <use xlink:href="./assets/img/icons/height-prio-icon.svg#height-prio-icon" fill="red"></use>
+                </svg>
+              </div>
+        </div>
+    </div>`;
+}
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -101,3 +124,21 @@ function highlight(id) {
 function removeHighlight(id) {
     document.getElementById(id).classList.remove('drag-area-highlight');
 }
+
+
+
+let subtaskCount = 2; // Anzahl der Subtasks
+let progressWidth = (1 / subtaskCount) * 100; // Breite der Fortschrittsanzeige in Prozent
+document.getElementById('myBar').style.width = progressWidth + '%';
+
+
+function setProgress(value) {
+    // Stelle sicher, dass der Wert zwischen 0 und 100 liegt
+    value = Math.max(0, Math.min(100, value));
+    
+    // Setze den Wert der Fortschrittsanzeige
+    document.getElementById('progress').style.width = value + "50%";
+}
+
+// Setze die Fortschrittsanzeige auf 50% (1/2)
+setProgress(50);
