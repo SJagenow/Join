248f37e5cdf559410371005146668b43f5210640
HananeSabil <sabil.h@gmx.de>
2024-02-15 02:45:47 +0100

    responsive summary.html

--- assets/js/summary.js
+++ assets/js/summary.js
@@ -1,38 +1,35 @@
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 
+function initSummary() {
+    getCurrentUser();
+    updateGreeting();
+
+}
 function getCurrentUser() {
     let userName = JSON.parse(localStorage.getItem("currentUserName"));
-    document.getElementById('summary-userName').innerHTML = userName;
+    let summaryUserName = document.getElementById('summary-userName');
     console.log(userName);
+
+    if (userName) {
+        summaryUserName.textContent = userName;
+      } else {
+        summaryUserName.textContentt = "Guest";
+      } 
 }
 
 
-function getCurrentDate() {
-    let year = new Date().getFullYear();
-    let month = new Date().getMonth();
-    let day = new Date().getDate();
-    let hour = new Date().getHours();
-    
-    document.getElementById('greet').innerHTML = getGreetings(hour);
-    document.getElementById('deadline-date').innerHTML = getFullDate(month, day, year);
-    console.log(hour);
-}
 
 
-function getGreetings(hour) {
-   console.log(hour);
-    if (hour > 5 && hour < 11) return 'Good morning,';
-    if (hour > 11 && hour < 18) return 'Good afternoon,';
-    else  return 'Good evening,';
-    
-} 
-
-// 
-function getFullDate(month, day, year) {
-   console.log(7);
-    let m = months[month];
-    let d = day;
-    let y = year;
-    return `${m} ${d}, ${y}`;
-}
+function updateGreeting() {
+    let greetingTime = document.getElementById("greet");
+    let currentDate = new Date();
+    let currentHour = currentDate.getHours();
+  
+    if (currentHour >= 5 && currentHour < 12) {
+      greetingTime.textContent = "Good Morning,";
+    } else if (currentHour >= 12 && currentHour < 18) {
+      greetingTime.textContent = "Good Afternoon,";
+    } else {
+      greetingTime.textContent = "Good Evening,";
+    }
+  }
