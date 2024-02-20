41aa5502831d5a8b825d7a37879bbe6faad7cec6
Pascal Thelke <p.thelke@googlemail.com>
2024-02-20 18:54:33 +0100

    all jsdoc commentary implemented

--- assets/js/storage.js
+++ assets/js/storage.js
@@ -2,29 +2,33 @@ const STORAGE_TOKEN = 'QDZWF0M731P6BJUN86LFPQFS6VZ8PUSF08W8Y1A2';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 
 
+/**
+ * Sets an item in the storage with the specified key and value.
+ * @param {string} key - The key under which to store the value.
+ * @param {string} value - The value to be stored.
+ * @returns {Promise} - A Promise that resolves with the result of the operation.
+ */
 async function setItem(key, value) {
-    // Erstellt ein Payload-Objekt mit Schlüssel, Wert und dem STORAGE_TOKEN
     const payload = { key, value, token: STORAGE_TOKEN };
-    // Sendet einen POST-Request an STORAGE_URL mit dem Payload als JSON-Daten
-    // und wartet auf die Antwort
     return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
-        // Wandelt die Antwort in JSON um und gibt das Ergebnis zurück
         .then(res => res.json());
 }
 
+
+/**
+ * Retrieves the value associated with the specified key from the storage.
+ * @param {string} key - The key of the item to retrieve.
+ * @returns {Promise} - A Promise that resolves with the retrieved value.
+ * @throws {string} - Throws an error if the data with the specified key is not found.
+ */
 async function getItem(key) {
-    // Erstellt die URL für den GET-Request mit dem Schlüssel und dem STORAGE_TOKEN
     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-    // Sendet einen GET-Request an die generierte URL und wartet auf die Antwort
     return fetch(url)
-        // Wandelt die Antwort in JSON um
         .then(res => res.json())
-        // Verarbeitet die Antwort und gibt den Wert des Schlüssels zurück, falls vorhanden
         .then(res => {
-            if (res.data) { // Wenn Daten vorhanden sind
-                return res.data.value; // Gib den Wert des Schlüssels zurück
+            if (res.data) { 
+                return res.data.value;
             }
-            // Andernfalls wirf einen Fehler
             throw `Could not find data with key "${key}".`;
         });
 }
